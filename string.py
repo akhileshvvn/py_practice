a6ad02e2e3398c90229ededc498cb4285aa6be2f
Akhilesh VVN <akhileshvvn@gmail.com>
2025-10-25 13:09:21 +0530

    string_counter_example
    Add anagram checking functionality using Counter
    
    This update introduces a method to determine if two strings, `s` and `t`, are anagrams. It utilizes the `Counter` class from the `collections` module to count character occurrences in both strings. The character counts are compared, and the result is printed as `True` or `False`. Additionally, the sorted versions of both strings are printed for verification.

---
+++ string.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+
+
+s = "anagram"
+
+count_s = Counter(s)
+
+
+
+t="nagaram"
+
+count_t = Counter(t)
+
+
+for a,b in count_t.items():
+    if count_s[a] != b:
+        print(False)
+        break
+else:
+    print(True)
+
+print(sorted(s) == sorted(t))
+print(sorted(s))
